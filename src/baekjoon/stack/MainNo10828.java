bc50a596c589b112b7a4f87a99337bc4a2d75b0a
nimosarang <sonjkzzang@gmail.com>
2023-02-21 23:57:11 +0900

    1월 24일 백준 알고리즘

---
+++ src/baekjoon/stack/MainNo10828.java
@@ -0,0 +1,22 @@
+package baekjoon.stack;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class MainNo10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+
+        int N = Integer.parseInt(bf.readLine());
+
+//        Stack<Integer> stack = new Stack<Integer>();
+
+
+
+
+    }
+}
